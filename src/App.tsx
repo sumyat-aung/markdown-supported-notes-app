31b87044ad26baf09392d237ca22323be3e5d114
sumyataung2004 <sumyataung1584@gmail.com>
2022-12-20 01:14:29 +0630

    set-up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const firstName: string = "App";
+
+  return (
+    <div className="App">
+      <div>{firstName}</div>
+    </div>
+  );
+}
+
+export default App;
